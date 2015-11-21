21fe65ae22d7aa2902577807ed594ff350e9ef2c
braelan <bbraelan@gmail.com>
2015-11-21 22:17:36 +0000

    changed timezone in pyAn

--- news/models.py
+++ news/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 
 
 
@@ -7,11 +6,10 @@ class Post(models.Model):
   author = models.ForeignKey('auth.User')
   title = models.CharField(max_length=200)
   text = models.TextField()
-  created_date = models.DateTimeField( default=timezone.now)
+  created_date = models.DateTimeField( blank= True, null=True)
   published_date = models.DateTimeField( blank= True, null=True)
 
   def publish(self):
-      self.published_date = timezone.now()
       self.save()
 
   def __str__(self):
