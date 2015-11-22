7f27e127827bd6f24e6bfcff4d3259e81444a115
Braelan <bbraelan@gmail.com>
2015-11-21 17:34:53 -0800

    added a home route

--- famiasnews/settings.py
+++ famiasnews/settings.py
@@ -58,13 +58,16 @@ WSGI_APPLICATION = 'famiasnews.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'famia$newsdb',
-        'USER': 'famia',
-        'PASSWORD': 'booker',
-        'HOST': 'mysql.server',
-    }
+
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'newsdb',
+            'USER': 'root',
+            'PASSWORD': 'booker',
+            'HOST': '',
+            'PORT': '',
+        }
+
 }
 
 # Internationalization
