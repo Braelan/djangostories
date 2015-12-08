e6145773fd5561d3fb485f9977e85e94db0500cf
Braelan <bbraelan@gmail.com>
2015-12-08 15:13:45 -0800

    api/images/.jpg in path

---
+++ news/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+class PostSerializer(serializers.Serializer):
+    author = serializers.StringRelatedField()
+    title = serializers.CharField(max_length=200)
+    text = serializers.CharField(max_length= None, min_length = None, allow_blank = False)
+    image = serializers.ImageField( max_length = None)
+    subtitle = serializers.CharField(allow_blank=True)
+    created_date = serializers.DateTimeField(format = None, input_formats = None)
+    published_date = serializers.DateTimeField(format = None, input_formats = None)
