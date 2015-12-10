eae1791b3c9c6059c2da6ad498370f1efed6af72
braelan <bbraelan@gmail.com>
2015-12-10 23:53:05 +0000

    gitignore

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import resolve
+from news.views import post_list
+from django.http import HttpRequest
 # Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_index_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, post_list)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = post_list(request)
+        self.assertIn(b'<html>', response.content)
+        self.assertIn(b'<title>Famia Nkansa</title>', response.content)
+        self.assertIn(b'</html>', response.content)
