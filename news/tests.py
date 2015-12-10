ec0685a7fa37e6695657477433ca1edce6514e5e
Braelan <bbraelan@gmail.com>
2015-12-10 15:42:54 -0800

    favicon and tests

--- news/tests.py
+++ news/tests.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import resolve
+from news.views import post_list
+from django.http import HttpRequest
 # Create your tests here.
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_smoke(self):
-        self.assertEqual(1+1, 3)
+    def test_root_url_resolves_to_index_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, post_list)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = post_list(request)
+        self.assertIn(b'<html>', response.content)
+        self.assertIn(b'<title>Famia Nkansa</title>', response.content)
+        self.assertIn(b'</html>', response.content)
