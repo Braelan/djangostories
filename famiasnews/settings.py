2de9e54a00fe37531fb10bea8a5488862641bdea
Braelan <bbraelan@gmail.com>
2016-01-01 15:13:38 -0800

    change settings.py

--- famiasnews/settings.py
+++ famiasnews/settings.py
@@ -60,22 +60,22 @@ WSGI_APPLICATION = 'famiasnews.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 #
 DATABASES = {
-        # 
-        # 'default': {
-        #     'ENGINE': 'django.db.backends.mysql',
-        #     'NAME': 'famia$newsdb',
-        #     'USER': 'famia',
-        #     'PASSWORD': 'booker',
-        #     'HOST': 'mysql.server',
-        # },
-       'default': {
-          'ENGINE': 'django.db.backends.mysql',
-          'NAME': 'newsdb',
-           'USER': 'root',
-           'PASSWORD': 'booker',
-           'HOST': '',
-           'PORT': '',
-               },
+
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'famia$newsdb',
+            'USER': 'famia',
+            'PASSWORD': 'booker',
+            'HOST': 'mysql.server',
+        },
+    #    'default': {
+    #       'ENGINE': 'django.db.backends.mysql',
+    #       'NAME': 'newsdb',
+    #        'USER': 'root',
+    #        'PASSWORD': 'booker',
+    #        'HOST': '',
+    #        'PORT': '',
+    #            },
 
           }
 
